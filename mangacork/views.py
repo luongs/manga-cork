bfe8afeddb85f22e3e703611c4b46a5652b06f05
Sebastien Luong <sebastienluong@gmail.com>
2015-11-29 09:22:51 -0800

    Use routes to map to an image url

--- mangacork/views.py
+++ mangacork/views.py
@@ -2,7 +2,17 @@ from flask import render_template
 
 from mangacork import app
 
+INDEX_IMAGE_PATH = 'dragonball_ch1/ndragon_ball_v001-000.jpg'
+FILE_TYPE = 'jpg'
+
 
 @app.route('/')
 def index():
-    return render_template('manga.html')
+    return render_template('manga.html', image_path=INDEX_IMAGE_PATH)
+
+
+@app.route('/<chapter>/<page>')
+def display():
+    image_path = '{}/{}.{}'.format(
+        chapter, page, FILE_TYPE)
+    return render_template('manga.html', image_path=image_path)
