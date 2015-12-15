57689b95b0c73e76d3c6f687d19973f213c7955b
ma3lstrom <sebastienluong@gmail.com>
2015-12-15 12:39:12 -0800

    Add first test

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,7 +1,7 @@
 from mangacork import utils
 
-def func(x):
-        return x+1
-
-def test_answer():
-        assert func(3) == 5
+def test_build_img_path():
+        chapter = "chapter1"
+        page = "3"
+        expected_output = "/chapter1/3"
+        assert utils.build_img_path(chapter,page) == expected_output
