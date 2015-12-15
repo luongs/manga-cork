7c7b3f5bf18e9fb81b2c85cc13e4d997ef8e318b
ma3lstrom <sebastienluong@gmail.com>
2015-12-15 12:57:09 -0800

    Use fixture instead

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,7 +1,14 @@
+import pytest
+
 from mangacork import utils
 
-def test_build_img_path():
-        chapter = "chapter1"
-        page = "3"
+@pytest.fixture
+def sample_page():
+    sample_page = {'chapter': "chapter1", 'page': 3}
+    return sample_page
+
+def test_build_img_path(sample_page):
+        chapter = sample_page["chapter"]
+        page = sample_page["page"]
         expected_output = "/chapter1/3"
         assert utils.build_img_path(chapter,page) == expected_output
