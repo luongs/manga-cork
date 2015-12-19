6e79a5d5563452abc79b223d4eed4377ef1c4fec
ma3lstrom <sebastienluong@gmail.com>
2015-12-18 17:13:13 -0800

    Add models

--- mangacork/__init__.py
+++ mangacork/__init__.py
@@ -2,10 +2,12 @@ import os
 import logging
 
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
 
 log = logging.getLogger(__name__)
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
+db = SQLAlchemy(app)
 
 import mangacork.views
---
+++ mangacork/models.py
@@ -0,0 +1,14 @@
+from app import db
+
+class LastPage(db.model):
+    __tablename__ = 'lastpage'
+
+    id = db.Column(db.Integer, primary_key=True)
+    lastpage = db.Column(db.String(80), unique=True)
+
+    def __init__(self, lastpage):
+        self.lastpage = lastpage
+
+    def __repr__(self):
+        return '<Lastpage {}>'.format(self.lastpage)
+
