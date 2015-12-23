38e3419a039ff605f636ac5e9e626e154c584ad7
ma3lstrom <sebastienluong@gmail.com>
2015-12-22 22:10:38 -0800

    Read values from db and log

--- mangacork/views.py
+++ mangacork/views.py
@@ -1,11 +1,21 @@
+import logging
+
 from flask import render_template
 
 from . import app
 from .utils import (increment_page_number, build_img_path, is_last_page)
+from .models import LastPage
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
 
 INDEX_CHAPTER = 'dragonball_ch1'
 INDEX_PAGE = 'ndragon_ball_v001-000'
-
+logger.info('Initialize LAST_PAGE_LIST from database')
+LAST_PAGE_LIST = LastPage.query.all()
+logger.debug('Last Page List {}'.format(LAST_PAGE_LIST))
+for i in LAST_PAGE_LIST:
+    logger.debug(i.lastpage)
 
 @app.route('/')
 def index():
