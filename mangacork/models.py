5bae91253930ece18290293921daa97c3c847183
ma3lstrom <sebastienluong@gmail.com>
2015-12-29 12:55:26 -0800

    Add comments table

--- mangacork/models.py
+++ mangacork/models.py
@@ -12,3 +12,18 @@ class LastPage(db.Model):
     def __repr__(self):
         return '<Lastpage {}>'.format(self.lastpage)
 
+class Comments(db.Model):
+    __tablename__ = 'comments'
+
+    id = db.Column(db.Integer, primary_key=True)
+    comment = db.Column(db.String(260))
+    page = db.Column(db.String(80))
+    chapter = db.Column(db.String(80))
+
+    def __init__(self, comment, page, chapter):
+        self.comment = comment
+        self.page = page
+        self.chapter = chapter
+
+    def __repr__(self):
+        return '<Comment {}>'.format(self.comment)
