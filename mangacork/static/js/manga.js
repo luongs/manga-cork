b7885b3f37ddc1462d6b4df86fb70c6fbd1173ce
ma3lstrom <sebastienluong@gmail.com>
2015-12-29 18:39:36 -0800

    Toggle comment and post section on off

--- mangacork/static/js/manga.js
+++ mangacork/static/js/manga.js
@@ -8,29 +8,47 @@ if (!localStorage.getItem('show_post_text') &&
 
 function toggle_post()
 {
-        if (localStorage.getItem('show_post_text') == 'hidden')
+        if (localStorage.getItem('show_post_text') == 'hidden'){
                 localStorage.setItem('show_post_text', 'visible');
+                show_post_section();
+        }
         else
+        {
                 localStorage.setItem('show_post_text', 'hidden');
-    alert("post local storage "+localStorage.getItem('show_post_text'));
+                hide_post_section();
+        }
 }
 
 function toggle_comments()
 {
-    if (localStorage.getItem('show_comments') == 'hidden')
+        if (localStorage.getItem('show_comments') == 'hidden')
+        {
                 localStorage.setItem('show_comments', 'visible');
+                show_comments();
+        }
         else
+        {
                 localStorage.setItem('show_comments', 'hidden');
-    alert("comments local storage "+localStorage.getItem('show_comments'));
+               hide_comments(); 
+        }
+}
+
+function hide_post_section()
+{
+        document.getElementById('post_section').style.display = 'none';
 }
 
 function hide_comments()
 {
-    localStorage.setItem('show_comments', 'hide');
+        document.getElementById('comment_section').style.display = 'none';
 }
 
-function hide_post()
+function show_post_section()
 {
-    localStorage.setItem('show_post', 'hide');
+        document.getElementById('post_section').style.display = 'block';
 }
 
+function show_comments()
+{
+        document.getElementById('comment_section').style.display = 'block';
+}
