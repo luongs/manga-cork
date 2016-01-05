27fa2d8cb6da2e9f50c600de7d1b4dda3fe3cb5c
ma3lstrom <sebastienluong@gmail.com>
2016-01-05 09:32:07 -0800

    Add email to user registration

--- mangacork/forms.py
+++ mangacork/forms.py
@@ -18,13 +18,19 @@ class LoginForm(Form):
 class SignupForm(Form):
     user_len_msg = 'Username cannot be longer than 25 characters'
     user_req_msg = 'Username must be filled'
+    user_dupl_msg = 'Username is already taken'
+    email_format_msg = 'Invalid email format'
+    email_req_msg = 'Email must be filled'
     pwd_req_msg = 'Password must be filled'
     pwd_len_msg = 'Password cannot be longer than 25 characters'
     pwd_match_msg = 'Passwords must match'
+
     username = TextField('Username', [validators.Length(max=25,
                                                         message=user_len_msg),
                           validators.Required(message=user_req_msg),
-                          Unique(User,User.username,message='Username taken')])
+                          Unique(User,User.username, message=user_dupl_msg)])
+    email = TextField('Email', [validators.Email(message=email_format_msg),
+                                validators.Required(message=email_req_msg)])
     password = StringField('Password', [validators.Length(max=25,
                                                           message=pwd_req_msg),
                            validators.InputRequired(message=pwd_req_msg),
