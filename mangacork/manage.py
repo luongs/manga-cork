7988a95aa707ece7a454ae4407b28a6cbda13343
ma3lstrom <sebastienluong@gmail.com>
2016-01-11 23:02:50 -0800

    Change call to environment

--- mangacork/manage.py
+++ mangacork/manage.py
@@ -3,7 +3,7 @@ from flask.ext.migrate import Migrate, MigrateCommand
 import os
 
 from mangacork import app, db
-app.config.from_object(os.environ['APP_SETTINGS'])
+app.config.from_object(os.getenv('APP_SETTINGS'))
 
 migrate = Migrate(app, db)
 manager = Manager(app)
